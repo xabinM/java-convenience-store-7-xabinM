e04f150c035ab85336c0b58bfda105abb0f9e551
Min <binine2@gmail.com>
2024-11-08 09:36:07 +0900

    feat(ProductService): 사용자 구매상품 입력 처리 서비스 구현

--- src/main/java/store/model/Inventory.java
+++ src/main/java/store/model/Inventory.java
@@ -3,4 +3,26 @@ package store.model;
 import java.util.List;
 
 public record Inventory(List<Product> inventory) {
+
+    public void updateProduct(int index, Product newProduct) {
+
+        inventory.set(index, newProduct);
+    }
+
+    public Product findProductByName(String name) {
+
+        return inventory.stream()
+                .filter(product -> product.compareName(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."));
+    }
+
+    public int findTotalStockByName(String name) {
+
+        return inventory.stream()
+                .filter(product -> product.compareName(name))
+                .mapToInt(Product::stock)
+                .sum();
+    }
+
 }
--- src/main/java/store/service/ProductService.java
+++ src/main/java/store/service/ProductService.java
@@ -1,5 +1,34 @@
 package store.service;
 
+import store.model.Inventory;
+import store.model.Product;
+import store.model.WishProduct;
+
+import java.util.List;
+
 public class ProductService {
+    private final Inventory inventory;
+
+    public ProductService(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public boolean checkIsPromotionProduct(WishProduct wishProduct) {
+        Product matchingProduct = inventory.findProductByName(wishProduct.name());
+
+        return matchingProduct.isExistPromotion();
+    }
+
+    public void checkTotalStock(WishProduct wishProduct) {
+        String name = wishProduct.name();
+        int quantity = wishProduct.quantity();
+
+        int totalStock = inventory.findTotalStockByName(name);
+
+        if (totalStock > quantity) {
+            //Promotion 적용 계산 메서드 호출 ( 2+1 인지, 1+1인지, 기간내인지)
+        }
+        throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    }
 
 }
