8563e80330ab56ba22e8a16842428f6244502003
Min <binine2@gmail.com>
2024-11-08 09:05:51 +0900

    refactor(view): static 제거 및 코드 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,35 +1,32 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.model.WishProduct;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class InputView {
-    public static List<Map<String, Integer>> requestPurchaseProduct() {
+    public List<WishProduct> requestPurchase() {
         String input = Console.readLine();
 
-        return parseInput(input.trim());
+        return listing(input.trim());
     }
 
-    public static List<Map<String, Integer>> parseInput(String input) {
-        List<Map<String, Integer>> result = new ArrayList<>();
+    private List<WishProduct> listing(String input) {
+        List<WishProduct> result = new ArrayList<>();
 
-        listing(input, result);
+        String[] parts = input.split(",");
+        for (String part : parts) {
+            result.add(parseWishProduct(part));
+        }
 
         return result;
     }
 
-    private static void listing(String input, List<Map<String, Integer>> result) {
-        String[] parts = input.split(",");
-        for (String part : parts) {
-            Map<String, Integer> itemDetail = new HashMap<>();
-            
-            String[] items = part.replaceAll("[\\[\\]]", "").split("-");
-            itemDetail.put(items[0], Integer.parseInt(items[1]));
-            result.add(itemDetail);
-        }
+    private WishProduct parseWishProduct(String part) {
+        String[] items = part.replaceAll("[\\[\\]]", "").split("-");
+
+        return new WishProduct(items[0], Integer.parseInt(items[1]));
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,32 +1,32 @@
 package store.view;
 
+import store.model.Inventory;
 import store.model.Product;
-
-import java.util.List;
 
 public class OutputView {
     private static final String PRINT_ENTRY_HELLO = "안녕하세요. W편의점입니다.";
     private static final String PRINT_ENTRY_INVENTORY = "현재 보유하고 있는 상품입니다.";
     private static final String REQUEST_PURCHASE_INPUT = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
 
-    public static void printEntry(List<Product> products) {
+    public void printEntry(Inventory inventory) {
         printWelcomeMessage();
         System.out.println();
-        printProductInventory(products);
+        printProductInventory(inventory);
     }
 
-    private static void printWelcomeMessage() {
+    private void printWelcomeMessage() {
         System.out.println(PRINT_ENTRY_HELLO);
         System.out.println(PRINT_ENTRY_INVENTORY);
     }
 
-    private static void printProductInventory(List<Product> products) {
-        for (Product product : products) {
+    private void printProductInventory(Inventory inventory) {
+        for (Product product : inventory.inventory()) {
             System.out.println(product.toString());
         }
     }
 
-    public static void printRequestPurchase() {
+    public void printRequestPurchase() {
         System.out.println(REQUEST_PURCHASE_INPUT);
     }
+
 }
