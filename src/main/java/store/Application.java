04933ab8211b85cdf704d875f6f70ca94b817d3b
Min <binine2@gmail.com>
2024-11-08 09:28:40 +0900

    refactor(Application): 구조 확립 및 코드 수정

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,10 +1,16 @@
 package store;
 
 import store.controller.ConvenienceStore;
+import store.model.Inventory;
+import store.model.InventoryLoader;
+import store.service.ProductService;
+import store.service.PromotionService;
 
 public class Application {
     public static void main(String[] args) {
-        ConvenienceStore convenienceStore = new ConvenienceStore();
+        Inventory inventory = new Inventory(new InventoryLoader().loadProductsFromFile());
+        ConvenienceStore convenienceStore = new ConvenienceStore(
+                new ProductService(inventory), new PromotionService(), inventory);
         convenienceStore.runStore();
     }
 }
