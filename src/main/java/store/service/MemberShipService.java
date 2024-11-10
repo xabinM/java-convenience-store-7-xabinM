c1f5b06c8f739ade085c36196459ffcae4ceb889
Min <binine2@gmail.com>
2024-11-11 04:20:50 +0900

    feat(MemberShipService): 멤버쉽 관련 로직 처리 서비스 구현

--- src/main/java/store/service/MemberShipService.java
+++ src/main/java/store/service/MemberShipService.java
@@ -1,4 +1,14 @@
 package store.service;
 
 public class MemberShipService {
+    public static int discountByMembership(int price) {
+
+        return (int) (price * 0.3);
+    }
+
+    public static int checkDiscountMaximum(int discountAmount) {
+
+        return Math.min(discountAmount, 8000);
+    }
 }
+
